ab690241089a2c520ea1f557d97e265d7b65d4ae
ThankGod Ossaija <codebugsolved@gmail.com>
2024-01-07 16:35:07 +0100

    calculate nodeDept

--- algos/node-depth.go
+++ algos/node-depth.go
@@ -3,7 +3,7 @@ package algos
 func NodeDepths(root *BinaryTree) int {
 	depths := []int{}
 
-	collectDepths(root, depths, 0)
+	depths = collectDepths(root, depths, 0)
 
 	sums := 0
 
@@ -14,13 +14,15 @@ func NodeDepths(root *BinaryTree) int {
 	return sums
 }
 
-func collectDepths(root *BinaryTree, depths []int, currentDepth int) {
+func collectDepths(root *BinaryTree, depths []int, currentDepth int) []int {
 	depths = append(depths, currentDepth)
 	currentDepth += 1
 	if root.Left != nil {
-		calculateSums(root.Left, depths, currentDepth)
+		depths = collectDepths(root.Left, depths, currentDepth)
 	}
 	if root.Right != nil {
-		calculateSums(root.Right, depths, currentDepth)
+		depths = collectDepths(root.Right, depths, currentDepth)
 	}
+
+	return depths
 }
